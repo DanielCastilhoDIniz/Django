dc5926fdce0567e7851163a5d276f27df23d7798
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-07-02 23:29:58 -0300

    tabelas e admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from myapp.models import Evento
 
 # Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_do_evento', 'data_da_criacao')
+    list_filter =('titulo','usuario',)
+
+
+admin.site.register(Evento, EventoAdmin)
