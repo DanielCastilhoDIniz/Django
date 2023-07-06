26a7b2134d239418889070bdc6f3954a18286044
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-07-05 22:38:56 -0300

    template para inserção da agenda

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
 from myapp.models import Evento
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # def index(request):
 #     return redirect('/agenda/')
 def login_user(request):
     return render(request, 'login.html')
+
 
 def submit_login(request):
     if request.POST:
@@ -17,11 +19,37 @@ def submit_login(request):
         if usuario is not None:
             login(request, usuario)
             return redirect('/')
-    else:
-        redirect('/')
+        else:
+            messages.error(request, "   Usuário ou senha inválido")
+
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
 
 @login_required(login_url='/login')
 def lista_eventos(request):
     evento = Evento.objects.all()
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_do_evento = request.POST.get('data_do_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_do_evento=data_do_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
